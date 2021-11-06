1559d1cfe3832f39745bbf4e4f7ab6d40420c6a2
Emerast-Green <kamil.sienkowski777@gmail.com>
2021-11-06 12:02:03 +0100

    1.2

--- ic2/energy_monitor.lua
+++ ic2/energy_monitor.lua
@@ -1,17 +1,18 @@
 local drivers = require("gehenna_drivers")
-local gpu = require("gpu")
+local gpu = require("component").gpu
 require("shell").execute("resolution 100 30")
 local math = require("math")
 
+gpu.fill(1,1,100,30," ")
 while true do
     n,t,p = drivers.ic2_te_mfsu.get_status()
     n = math.floor(n)
     t = math.floor(t)
     p = math.floor(p)
-    gpu.set(1,8,100,2," ")
+    gpu.fill(1,8,100,2," ")
     gpu.set(1,8,p.."%")
     gpu.set(1,9,n.."/"..t)
-    gpu.set(1,10,100,10,"|")
-    gpu.set(p,10,100-p,10," ")
+    gpu.fill(1,10,100,10,"|")
+    gpu.fill(p,10,101-p,10," ")
     os.sleep(.5)
 end
